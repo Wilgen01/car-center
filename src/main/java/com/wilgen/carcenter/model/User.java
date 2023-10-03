b80bd112fd57fbdc885fab5c6b74de36f8106461
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-03 14:22:14 -0500

    add security entities

---
+++ src/main/java/com/wilgen/carcenter/model/User.java
@@ -0,0 +1,26 @@
+package com.wilgen.carcenter.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = Role.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+}
---
+++ src/main/java/com/wilgen/carcenter/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wilgen.carcenter.repository;
+
+import com.wilgen.carcenter.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
