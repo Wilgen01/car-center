a08d4042f5d8d54cffa93fc03c2959430c45536a
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-05 15:38:33 -0500

    Translate error messages

--- src/main/java/com/wilgen/carcenter/service/impl/BrandServiceImpl.java
+++ src/main/java/com/wilgen/carcenter/service/impl/BrandServiceImpl.java
@@ -21,7 +21,7 @@ public class BrandServiceImpl implements BrandService {
         try {
             return brandRepository.findAll();
         } catch (Exception e) {
-            throw new Exception("unexpected error", e);
+            throw new Exception("Ha ocurrido un error inesperado", e);
         }
     }
 }
